05e57ce89714a897be792984e0cfbce7d4596cd3
1710099 <sathubian1999@gmail.com>
2018-05-21 11:07:50 +0700

    update frame

--- app/src/main/java/com/example/bm/werewolf/Activity/MainActivity.java
+++ app/src/main/java/com/example/bm/werewolf/Activity/MainActivity.java
@@ -1,20 +1,23 @@
-package com.example.bm.werewolf;
+package com.example.bm.werewolf.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bm.werewolf.Adapter.ViewPagerAdapter;
+import com.example.bm.werewolf.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @BindView(R.id.tv_screen_name)
     TextView tvScreenName;
@@ -33,21 +36,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        bottomNavigation.getMenu().getItem(0).getIcon().setAlpha(100);
+        bottomNavigation.getMenu().getItem(1).getIcon().setAlpha(255);
+        bottomNavigation.getMenu().getItem(2).getIcon().setAlpha(100);
+        bottomNavigation.getMenu().getItem(3).getIcon().setAlpha(100);
+        bottomNavigation.setItemIconTintList(null);
         bottomNavigation.setSelectedItemId(R.id.item_play);
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                bottomNavigation.getMenu().getItem(0).getIcon().setAlpha(100);
+                bottomNavigation.getMenu().getItem(1).getIcon().setAlpha(100);
+                bottomNavigation.getMenu().getItem(2).getIcon().setAlpha(100);
+                bottomNavigation.getMenu().getItem(3).getIcon().setAlpha(100);
                 switch (item.getItemId()) {
                     case R.id.item_friends:
+                        bottomNavigation.getMenu().getItem(0).getIcon().setAlpha(255);
                         viewPager.setCurrentItem(0);
                         break;
                     case R.id.item_play:
+                        bottomNavigation.getMenu().getItem(1).getIcon().setAlpha(255);
                         viewPager.setCurrentItem(1);
                         break;
                     case R.id.item_achievement:
+                        bottomNavigation.getMenu().getItem(2).getIcon().setAlpha(255);
                         viewPager.setCurrentItem(2);
                         break;
                     case R.id.item_user:
+                        bottomNavigation.getMenu().getItem(3).getIcon().setAlpha(255);
                         viewPager.setCurrentItem(3);
                         break;
                 }
@@ -66,6 +82,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 bottomNavigation.getMenu().getItem(position).setChecked(true);
+                switch (position) {
+                    case 0:
+                        tvScreenName.setText("Friends list");
+                        break;
+                    case 1:
+                        tvScreenName.setText("Lobby");
+                        break;
+                    case 2:
+                        tvScreenName.setText("Achievement");
+                        break;
+                    case 3:
+                        tvScreenName.setText("Your profile");
+                        break;
+                }
             }
 
             @Override
