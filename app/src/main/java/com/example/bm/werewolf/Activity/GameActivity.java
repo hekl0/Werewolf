8e251ba898dc7c7cffa70a4c7f4d9f679fe7af1d
1710099 <sathubian1999@gmail.com>
2018-05-28 19:52:13 +0700

    add waiting room

---
+++ app/src/main/java/com/example/bm/werewolf/Activity/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.bm.werewolf.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bm.werewolf.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
