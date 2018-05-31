48d0c0517bd2887b7c881059329e2d2c7eb13deb
Trung Đào <daoquangtrung1998@outlook.com.vn>
2018-05-31 22:11:52 +0700

    Finished Friend Fragment, showing online friends

--- app/src/main/java/com/example/bm/werewolf/Adapter/FriendsExpandableListViewAdapter.java
+++ app/src/main/java/com/example/bm/werewolf/Adapter/FriendsExpandableListViewAdapter.java
@@ -1,40 +1,58 @@
 package com.example.bm.werewolf.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bm.werewolf.Model.UserModel;
+import com.example.bm.werewolf.R;
+
+import java.util.List;
 
 public class FriendsExpandableListViewAdapter extends BaseExpandableListAdapter {
 
+    Context context;
+    List<String> groupName;
+    List<UserModel> childUser;
+
+    public FriendsExpandableListViewAdapter(Context context, List<String> groupName, List<UserModel> childUser) {
+        this.context = context;
+        this.groupName = groupName;
+        this.childUser = childUser;
+    }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return groupName.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return childUser.size();
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return groupName.get(groupPosition);
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return childUser.get(childPosition);
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return childPosition;
     }
 
     @Override
@@ -44,16 +62,38 @@ public class FriendsExpandableListViewAdapter extends BaseExpandableListAdapter 
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        convertView = inflater.inflate(R.layout.item_list_friends, parent, false);
+        ImageView ivArrow = convertView.findViewById(R.id.iv_arrow);
+
+        if (isExpanded) {
+            ivArrow.setImageResource(R.drawable.ic_keyboard_arrow_up_white_24dp);
+        } else {
+            ivArrow.setImageResource(R.drawable.ic_keyboard_arrow_down_white_24dp);
+        }
+
+        return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        convertView = inflater.inflate(R.layout.item_list_player, parent, false);
+
+        UserModel user = (UserModel) getChild(0, childPosition);
+
+        TextView tvPlayerName = convertView.findViewById(R.id.tv_player);
+        ImageView ivOnline = convertView.findViewById(R.id.iv_online);
+
+        tvPlayerName.setText(user.name);
+        ivOnline.setImageResource((user.isOnline) ? R.drawable.ic_grid_world_green : R.drawable.ic_grid_world_red);
+
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
 }
--- app/src/main/java/com/example/bm/werewolf/Fragment/FriendsFragment.java
+++ app/src/main/java/com/example/bm/werewolf/Fragment/FriendsFragment.java
@@ -7,15 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 import com.example.bm.werewolf.Activity.WaitingRoomActivity;
+import com.example.bm.werewolf.Adapter.FriendsExpandableListViewAdapter;
+import com.example.bm.werewolf.Model.UserModel;
 import com.example.bm.werewolf.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FriendsFragment extends Fragment {
 
+    ExpandableListView elvFriends;
+    FriendsExpandableListViewAdapter adapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    List<UserModel> childUser;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -26,10 +42,43 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        childUser = new ArrayList<>();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("User list");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                childUser.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    UserModel model = snapshot.getValue(UserModel.class);
+                    childUser.add(model);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        elvFriends = rootView.findViewById(R.id.elv_friends);
+        adapter = new FriendsExpandableListViewAdapter(
+                getContext(),
+                new ArrayList<String>() {
+                    {
+                        add("Friends");
+                    }
+                },
+                childUser
+        );
+        elvFriends.setAdapter(adapter);
+
         Intent intent = new Intent(getContext(), WaitingRoomActivity.class);
         startActivity(intent);
 
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
     }
+
 
 }
